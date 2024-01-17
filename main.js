3bbb88db0975fde94413a8d1741caeb41f01448b
ViraMykhailova <vera.madudina@gmail.com>
2024-01-17 15:19:04 +0200

    hw-3

---
+++ main.js
@@ -0,0 +1,11 @@
+let a = +prompt('Введіть перше число');
+
+let b = +prompt('Введіть друге число');
+
+let c = +prompt ('Введіть тертє число');
+
+let result = (a + b + c) / 3;
+
+console.log(result);
+
+alert(`Середнє значення дорівнює ${result}`)
