374de1f0f8133c468d2ab2e93a71584e03b0ac6d
ViraMykhailova <vera.madudina@gmail.com>
2024-01-31 15:52:26 +0200

    hw-12

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function pow (number, degree) {
+    if(degree === 0) {
+        return 1;
+    } else if(degree === 1) {
+        return number;
+    } else {
+        return number *= pow(number , degree - 1);
+    }
+
+}
+console.log(pow(8,7));
