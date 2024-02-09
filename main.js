716a36943d1893fb4aeb39c7fd69874d9d205265
ViraMykhailova <vera.madudina@gmail.com>
2024-02-09 15:58:05 +0200

    hw-16 update

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 function showDiv () {
 const showEl=document.querySelector('#empty');
-showEl.classList.add('displayBlock');
+showEl.classList.add('displayDiv');
 }
 
 function hideDiv () {
     const hideEl=document.querySelector('#empty');
-    hideEl.classList.remove('displayBlock');
+    hideEl.classList.remove('displayDiv');
 }
 document.querySelector('#textArea').addEventListener('focus', showDiv);
 document.querySelector('#textArea').addEventListener('blur', hideDiv);
