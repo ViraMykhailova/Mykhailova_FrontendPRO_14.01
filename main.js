9994b7990ef22cd8f76e6149d396361cd53a8f11
ViraMykhailova <vera.madudina@gmail.com>
2024-02-13 15:26:48 +0200

    hw-18 update

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ table.appendChild(tbody);
 let number = 1;
 for(let i = 0; i <10; i++) {
     const tr = document.createElement('tr');
-    for(let j =0; j < 10; j++) {
+    for(let j = 0; j < 10; j++) {
         number = Math.floor(Math.random() * 100 + 1);
         const td = document.createElement('td');
         td.classList.add('col');
         td.textContent = number;
         tr.appendChild(td);
     }
-    tbody.appendChild(tr)
+    tbody.appendChild(tr);
 }
