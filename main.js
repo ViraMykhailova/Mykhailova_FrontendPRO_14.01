7b27e512cc179df86755b32600cd1301c3b33501
ViraMykhailova <vera.madudina@gmail.com>
2024-02-21 16:13:34 +0200

    hw-22 draft

---
+++ main.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const smilesList = [{
+    smile:'😀',
+    vote_counter: 0,
+}, {
+    smile:'😈',
+    vote_counter: 0,
+    }, {
+    smile:'😊',
+    vote_counter: 0,
+    }, {
+    smile:'😎',
+    vote_counter: 0,
+    }, {
+    smile:'😜',
+    vote_counter: 0,
+    }
+];
+
+const wrapper = document.querySelector('#wrapper');
+let counter = 0;
+smilesList.forEach(smile =>{
+    const smileVoteWrapper = document.createElement('div');
+    smileVoteWrapper.classList.add('smile-vote');
+    const smileDiv = document.createElement('div');
+    smileDiv.textContent = smile.smile;
+    smileDiv.classList.add('smileImg');
+
+    const voteCount = document.createElement('p');
+    voteCount.textContent = smile.vote_counter;
+
+
+    wrapper.appendChild(smileVoteWrapper);
+    smileVoteWrapper.appendChild(smileDiv);
+    smileVoteWrapper.appendChild(voteCount);
+
+    smileDiv.addEventListener('click', () =>{
+        counter++;
+        smile.vote_counter = counter;
+        voteCount.textContent = smile.vote_counter;
+
+    })
+
+})
+
